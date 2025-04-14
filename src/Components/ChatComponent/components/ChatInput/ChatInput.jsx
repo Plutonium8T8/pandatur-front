332a40c8cd2833e2b3832550c764fb0dc0a7997e
DanNuta <dany.condurari@gmai.com>
2025-04-14 20:20:22 +0300

    fix(GroupMessages): duplicate key

--- src/Components/ChatComponent/components/ChatInput/ChatInput.jsx
+++ src/Components/ChatComponent/components/ChatInput/ChatInput.jsx
@@ -67,7 +67,7 @@ export const ChatInput = ({
     <>
       <Box p="16px">
         <Flex w="100%" gap="xs" mb="xs" align="center">
-          {socialMediaIcons[currentClient.payload?.platform] && (
+          {currentClient.payload?.platform && (
             <Flex>{socialMediaIcons[currentClient.payload?.platform]}</Flex>
           )}
           <Select
