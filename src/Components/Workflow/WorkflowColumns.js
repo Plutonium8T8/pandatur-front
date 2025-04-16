995240f984489ba058cb25cfdcd1d3dbdac3d35f
DanNuta <dany.condurari@gmai.com>
2025-04-16 09:47:01 +0300

    feat(WorkflowColumns): scroll right on drag

--- src/Components/Workflow/WorkflowColumns.js
+++ src/Components/Workflow/WorkflowColumns.js
@@ -1,7 +1,8 @@
 import { Flex } from "@mantine/core";
+import { useRef, useEffect } from "react";
 import { workflowOptions } from "../../FormOptions";
 import { WorkflowColumn } from "./components";
-import { useGetTechniciansList } from "../../hooks";
+import { useGetTechniciansList, useApp } from "../../hooks";
 
 export const WorkflowColumns = ({
   tickets,
@@ -11,9 +12,41 @@ export const WorkflowColumns = ({
   fetchTickets,
 }) => {
   const { technicians } = useGetTechniciansList();
+  const { isCollapsed } = useApp();
+
+  const wrapperColumnRef = useRef(null);
+  const isScrollRight = useRef(false);
+
+  const leftSpace = isCollapsed ? 79 : 249;
+
+  useEffect(() => {
+    wrapperColumnRef.current.addEventListener("mouseleave", () => {
+      isScrollRight.current = false;
+    });
+
+    wrapperColumnRef.current.addEventListener("mousemove", (e) => {
+      if (!isScrollRight.current) return;
+      e.preventDefault();
+      wrapperColumnRef.current.scrollLeft = e.pageX - leftSpace;
+    });
+
+    wrapperColumnRef.current.addEventListener("mouseup", () => {
+      isScrollRight.current = false;
+    });
+
+    wrapperColumnRef.current.addEventListener("mousedown", (e) => {
+      isScrollRight.current = true;
+    });
+  }, []);
 
   return (
-    <Flex gap="xs" w="100%" h="100%" className="overflow-x-scroll">
+    <Flex
+      ref={wrapperColumnRef}
+      gap="xs"
+      w="100%"
+      h="100%"
+      className="overflow-x-scroll"
+    >
       {workflowOptions
         .filter((workflow) => selectedWorkflow.includes(workflow))
         .map((workflow) => (
