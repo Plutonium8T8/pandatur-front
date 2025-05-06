ac0552414c5189bb0edb2f7969da4427028738a1
maksimbordan <bordan.maxim@gmail.com>
2025-05-06 10:44:54 +0300

    fix background and name level

--- src/Components/utils/permissionConstants.js
+++ src/Components/utils/permissionConstants.js
@@ -13,7 +13,7 @@ export const actions = ["CREATE", "VIEW", "EDIT", "DELETE"];
 export const levels = [
     { label: "Denied", value: "Denied", color: "#fa5252" },
     { label: "If responsible", value: "IfResponsible", color: "#fab005" },
-    { label: "Team-wide access", value: "Team", color: "#228be6" },
+    { label: "Team", value: "Team", color: "#228be6" },
     { label: "Allowed", value: "Allowed", color: "#40c057" },
 ];
 
