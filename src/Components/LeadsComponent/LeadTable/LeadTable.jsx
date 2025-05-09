1845d92304de3923769e7eed9e2492503f5150f3
maksimbordan <bordan.maxim@gmail.com>
2025-05-09 13:17:14 +0300

    fix

--- src/Components/LeadsComponent/LeadTable/LeadTable.jsx
+++ src/Components/LeadsComponent/LeadTable/LeadTable.jsx
@@ -349,8 +349,8 @@ export const LeadTable = ({
         return (
           <Paper pos="absolute" top="0" right="0" bottom="0" shadow="xs" w="100%">
             <Flex align="center" justify="center" gap="8" h="100%" p="xs">
-              <Can permission={{ module: "leads", action: "delete" }}>
-                <ActionIcon variant="danger" onClick={() => handleDeleteLead(ticket.id)}>
+            <Can permission={{ module: "leads", action: "delete" }} context={{ responsibleId }}>
+            <ActionIcon variant="danger" onClick={() => handleDeleteLead(ticket.id)}>
                   <MdDelete />
                 </ActionIcon>
               </Can>
