2a408bb21787dd8f7b8f3b5b6c2641fa726c3da0
maksimbordan <bordan.maxim@gmail.com>
2025-05-22 14:56:12 +0300

    enable system filter workflow

--- src/Components/LeadsComponent/LeadsKanbanFilter.jsx
+++ src/Components/LeadsComponent/LeadsKanbanFilter.jsx
@@ -1,33 +1,21 @@
-import { Tabs, Flex, Button } from "@mantine/core";
-import { useEffect, useState } from "react";
-import { SelectWorkflow } from "../SelectWorkflow";
+import { Tabs } from "@mantine/core";
 import { getLanguageByKey } from "../utils";
 import { TicketFormTabs } from "../TicketFormTabs";
-import { filteredWorkflows } from "./utils";
 import { MessageFilterForm } from "./MessageFilterForm";
 
 export const LeadsKanbanFilter = ({
   onClose,
-  onApplyWorkflowFilters,
   loading,
-  systemWorkflow: baseSystemWorkflow,
   initialData,
   onSubmitTicket,
 }) => {
-  const [systemWorkflow, setSystemWorkflow] = useState(filteredWorkflows);
-
-  useEffect(() => {
-    setSystemWorkflow(baseSystemWorkflow);
-  }, [baseSystemWorkflow]);
-
   return (
-    <Tabs h="100%" className="leads-modal-filter-tabs" defaultValue="filter_workflow">
+    <Tabs
+      h="100%"
+      className="leads-modal-filter-tabs"
+      defaultValue="filter_ticket"
+    >
       <Tabs.List>
-        {onApplyWorkflowFilters && (
-          <Tabs.Tab value="filter_workflow">
-            {getLanguageByKey("Filtru de sistem")}
-          </Tabs.Tab>
-        )}
         <Tabs.Tab value="filter_ticket">
           {getLanguageByKey("Filtru pentru Lead")}
         </Tabs.Tab>
@@ -35,25 +23,6 @@ export const LeadsKanbanFilter = ({
           {getLanguageByKey("Filtru dupǎ mesaje")}
         </Tabs.Tab>
       </Tabs.List>
-
-      {onApplyWorkflowFilters && (
-        <Tabs.Panel value="filter_workflow" pt="xs">
-          <Flex direction="column" justify="space-between" h="100%">
-            <SelectWorkflow selectedValues={systemWorkflow} onChange={setSystemWorkflow} />
-            <Flex justify="end" gap="md" mt="md">
-              <Button variant="outline" onClick={() => onSubmitTicket({}, "message")}>
-                {getLanguageByKey("Reset filter")}
-              </Button>
-              <Button variant="default" onClick={onClose}>
-                {getLanguageByKey("Închide")}
-              </Button>
-              <Button variant="filled" loading={loading} onClick={() => onSubmitTicket({}, "message")}>
-                {getLanguageByKey("Aplică")}
-              </Button>
-            </Flex>
-          </Flex>
-        </Tabs.Panel>
-      )}
 
       <Tabs.Panel value="filter_ticket" pt="xs">
         <TicketFormTabs
