303e07d3faf2a7c8cb745e7e028ee961f6f93011
maksimbordan <bordan.maxim@gmail.com>
2025-06-02 16:15:51 +0300

    new api and new field

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,5 @@
 export * from "./PageHeader";
 export * from "./Spin";
-export * from "./Logs";
 export * from "./TicketFormTabs";
 export * from "./SelectWorkflow";
 export * from "./MantineModal";
