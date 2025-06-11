4457ac181e5b913c40587991480b2abf80359ae4
maksimbordan <bordan.maxim@gmail.com>
2025-06-11 12:33:22 +0300

    color icon messanger

--- src/Components/utils/socialMediaIcons.js
+++ src/Components/utils/socialMediaIcons.js
@@ -7,9 +7,9 @@ import {
 } from "react-icons/fa";
 
 export const socialMediaIcons = {
-  facebook: <FaFacebook />,
-  instagram: <FaInstagram />,
-  whatsapp: <FaWhatsapp />,
-  viber: <FaViber />,
-  telegram: <FaTelegram />,
+  facebook: <FaFacebook color="blue" />,
+  instagram: <FaInstagram color="#d62976" />,
+  whatsapp: <FaWhatsapp color="green" />,
+  viber: <FaViber color="violet" />,
+  telegram: <FaTelegram color="#0088cc" />,
 };
