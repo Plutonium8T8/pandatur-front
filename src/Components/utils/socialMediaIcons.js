64e97dcc5a55430c1b014e88af1a9c1e261691bb
maksimbordan <bordan.maxim@gmail.com>
2025-10-09 16:41:38 +0300

    social icons

--- src/Components/utils/socialMediaIcons.js
+++ src/Components/utils/socialMediaIcons.js
@@ -6,10 +6,47 @@ import {
   FaTelegram,
 } from "react-icons/fa";
 
+const IconWrapper = ({ children, bgColor }) => (
+  <div
+    style={{
+      display: "inline-flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "24px",
+      height: "24px",
+      borderRadius: "12px",
+      backgroundColor: bgColor,
+      padding: "2px",
+    }}
+  >
+    {children}
+  </div>
+);
+
 export const socialMediaIcons = {
-  facebook: <FaFacebook color="blue" />,
-  instagram: <FaInstagram color="#d62976" />,
-  whatsapp: <FaWhatsapp color="green" />,
-  viber: <FaViber color="#59267c" />,
-  telegram: <FaTelegram color="#0088cc" />,
+  facebook: (
+    <IconWrapper bgColor="#1877F2">
+      <FaFacebook color="white" />
+    </IconWrapper>
+  ),
+  instagram: (
+    <IconWrapper bgColor="#C13584">
+      <FaInstagram color="white" />
+    </IconWrapper>
+  ),
+  whatsapp: (
+    <IconWrapper bgColor="#25D366">
+      <FaWhatsapp color="white" />
+    </IconWrapper>
+  ),
+  viber: (
+    <IconWrapper bgColor="#7360F2">
+      <FaViber color="white" />
+    </IconWrapper>
+  ),
+  telegram: (
+    <IconWrapper bgColor="#0088cc">
+      <FaTelegram color="white" />
+    </IconWrapper>
+  ),
 };
