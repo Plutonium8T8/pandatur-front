c460592d9f92cd044944703d7d0f43a99eb493ae
maksimbordan <bordan.maxim@gmail.com>
2025-10-10 11:32:35 +0300

    delete old api

--- src/api/messages.js
+++ src/api/messages.js
@@ -2,11 +2,6 @@ import { baseAxios } from "./baseAxios";
 import Cookies from "js-cookie";
 
 export const messages = {
-  list: async () => {
-    const { data } = await baseAxios.get("/api/messages");
-    return data;
-  },
-
   messagesTicketById: async (id) => {
     const { data } = await baseAxios.get(`/api/messages/ticket/${id}`);
     return data;
